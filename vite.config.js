025f0fc756c7257839e8733caf05d42ebaee7fcb
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-12-11 12:23:55 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.json'] // Add this to help Vite resolve file extensions
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'components': path.resolve(__dirname, './src/components')
+    }
   }
 });
