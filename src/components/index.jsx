e0bb92f3cae54b6089ce45765b8f5e9be2cf0886
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-12-11 15:05:15 -0600

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as StrengthWeaknessAnalysis } from './StrengthWeaknessAnalysis.
 export { default as CareerTimeline } from './CareerTimeline.jsx';
 export { default as QuadrantSkillsChart } from './QuadrantSkillsChart.jsx';
 export { default as CoreCompetenceWheel } from './CoreCompetenceWheel.jsx';
+export { default as AboutSection } from './AboutSection.jsx';
