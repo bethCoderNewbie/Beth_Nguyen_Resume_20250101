7265a631c5636cc641618e70b8969323720c50b6
bethCoderNewbie <155182607+bethCoderNewbie@users.noreply.github.com>
2024-12-11 12:22:58 -0600

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as StrengthWeaknessAnalysis } from './StrengthWeaknessAnalysis';
+export { default as CareerTimeline } from './CareerTimeline';
+export { default as QuadrantSkillsChart } from './QuadrantSkillsChart';
+export { default as CoreCompetenceWheel } from './CoreCompetenceWheel';
